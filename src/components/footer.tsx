b92a2c03c4144b165071ea4625e8a98ace845101
AlexTraveylan <monsieurdemares@gmail.com>
2023-08-16 23:23:40 +0200

    fix padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <div className="z-20 border-t border-gray-200">
-      <div className="flex justify-between max-w-screen-xl py-4 mx-auto">
+      <div className="flex justify-between max-w-screen-xl p-4 mx-auto">
         <h1>Version 0.0.1</h1>
         <h1>Alex Traveylan</h1>
       </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { SwitchTheme } from "./switch-theme"
 export function Header() {
   return (
     <div className="z-20 w-full border-b border-gray-200">
-      <div className="flex justify-between max-w-screen-xl py-4 mx-auto">
+      <div className="flex justify-between max-w-screen-xl p-4 mx-auto">
         <Image src="/snowprint_logo.png" width={30} height={30} alt="icon" />
         <SwitchTheme />
       </div>
